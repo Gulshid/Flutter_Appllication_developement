a3b6e80c010ecad8ed4b1bc9d3072076a78f4677
Gulshid <gulshidzada4@gmail.com>
2024-11-27 18:28:32 +0500

    Current time

--- Dart/task25.dart
+++ Dart/task25.dart
@@ -1,6 +1,3 @@
-import 'dart:math';
-
-
 void main() {
   int hour = DateTime.now().hour;
   String find_date_time = (hour < 12)
@@ -10,6 +7,5 @@ void main() {
           : "Good Evening";
   print("Now Date Time is :$find_date_time");
   
-  int random = Random().nextInt(20);
-  print("The random number is :$random");
+  
 }
