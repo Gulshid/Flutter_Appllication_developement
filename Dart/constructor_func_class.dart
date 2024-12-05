f9c15da1c4145bc4945f960655b262a1c72e6cb5
Gulshid Zada <gulshidzada4@gmail.com>
2024-12-05 11:01:27 +0500

    Constructor function in class

--- Dart/constructor_func_class.dart
+++ Dart/constructor_func_class.dart
@@ -10,7 +10,7 @@ class Student {
     double? CGPA = double.parse(stdin.readLineSync()!);
     print("Enter the Student Roll  No:");
     int? roll_no = int.parse(stdin.readLineSync()!);
-    print("Enter the Studenr Current Semester: ");
+    print("Enter the Student Current Semester: ");
     String? sems = stdin.readLineSync()!;
 
     print("\t\t\t*********************");
