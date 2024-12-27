d7c342d7eeb358dd1e32a99d6bd44e07a7f7142a
Gulshid Zada <gulshidzada4@gmail.com>
2024-12-27 14:37:43 +0500

    UI 13

--- UI/UX/flutter_application_1/lib/main.dart
+++ UI/UX/flutter_application_1/lib/main.dart
@@ -1,31 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_application_1/UI_11.dart';
-import 'package:flutter_application_1/UI_12.dart';
-//import 'package:flutter_application_1/practise.dart';
-//import 'package:flutter_application_1/UI_task11.dart';
-//import 'package:flutter_application_1/UI%207.dart';
-//import 'package:flutter_application_1/UI_task7.dart';
-//import 'package:flutter_application_1/UI_10.dart';
-//import 'package:flutter_application_1/UI_8.dart';
-//import 'package:flutter_application_1/UI_8.dart';
-//import 'package:flutter_application_1/UI_9.dart';
-//import 'package:flutter_application_1/square_listview.dart';
-//import 'package:flutter_application_1/UI_task5.dart';
-//import 'package:flutter_application_1/UI_task6.dart';
-//import 'package:flutter_application_1/UI%207.dart';
-//import 'package:flutter_application_1/UI_8.dart';
-//import 'package:flutter_application_1/UI_Button.dart';
-//import 'package:flutter_application_1/UI_task4.dart';
-//import 'package:flutter_application_1/UI_task5.dart';
-//import 'package:flutter_application_1/UI_task6.dart';
-//import 'package:flutter_application_1/UI_task3.dart';
-//import 'package:flutter_application_1/ui.dart';
-//import 'package:flutter_application_1/UI_task2.dart';
-//import 'package:flutter_application_1/UI_task3.dart';
-//import 'package:flutter_application_1/UI_task4.dart';
-//import 'package:flutter_application_1/UI_task2.dart';
-//import 'package:flutter_application_1/UI_task3.dart';
-//import 'package:flutter_application_1/UI_task1.dart';
+//import 'package:flutter_application_1/UI_12.dart';
+import 'package:flutter_application_1/UI_13.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -42,63 +18,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:UI_12(),
+      home:UI_13(),
       //  Task1(),
     );
 
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    var scaffold = Scaffold(
-      backgroundColor: const Color.fromARGB(255, 230, 207, 198),
-      appBar: AppBar(
-        
-        backgroundColor: const Color.fromARGB(255, 3, 72, 128),
-        
-        title: Center(child: Text("***Flutter Widget**")),
-        leading: Icon(Icons.menu ), 
-        actions: [Icon(Icons.person)],
-      ),
-
-      body: Center(
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-               
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 39, 201, 10),
-                  borderRadius: BorderRadius.circular(10),
-                  
-                  
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: 
-                
-                [
-                  Text('Hello! Container..',style: TextStyle(
-                    fontSize: 20,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                ],),
-                      
-              ),
-            ),
-            
-          ],
-        ),
-      )
-      
-    );
-    return scaffold;
   }
 }
