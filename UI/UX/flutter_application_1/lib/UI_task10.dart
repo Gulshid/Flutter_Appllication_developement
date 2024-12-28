a0a097af811d3c26a64b8d372ec1b01d152adbce
Gulshid Zada <gulshidzada4@gmail.com>
2024-12-28 20:31:41 +0500

    Task10

--- UI/UX/flutter_application_1/lib/UI_task10.dart
+++ UI/UX/flutter_application_1/lib/UI_task10.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class Task10 extends StatefulWidget {
@@ -12,17 +14,97 @@ class _Task10State extends State<Task10> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 5, 94, 85),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 200,vertical: 200),
-        child: Container(
-          height: 80,
-          width: 80,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 170,vertical: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+             CircleAvatar( backgroundImage: AssetImage('assets/img_2.jpg'),
+                   radius: 35,
+                   
+                   ),
+              
+          Text('Gulshid Zada',style: TextStyle(color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
           
-          child: CircleAvatar( backgroundImage: AssetImage('assets/my_image.png'),
-          radius: 20,
+          fontFamily: 'Pacifico'),
+          ),
+          SizedBox(height: 5,),
+          Text('Flutter Developer',style: TextStyle(color: const Color.fromARGB(255, 157, 153, 153),
+          //fontSize: 10,
+          letterSpacing: 2.5,
+          fontWeight: FontWeight.normal,
+          fontStyle: FontStyle.italic),
+          ),
+        Divider(),
+        SizedBox(height: 10,),
+        Container(
+          height:35,
+          width: 300,
+          
+          decoration: BoxDecoration(
+          color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [BoxShadow(
+              color: const Color.fromARGB(255, 15, 22, 220),
+                spreadRadius: 3,
+              blurRadius: 3,
+            )]
+          ),
+          
+          child:  Row(
+            children: [
+                SizedBox(width: 10,),
+              Icon(Icons.call,color: const Color.fromARGB(255, 3, 120, 108),),
+              SizedBox(width: 5,),
+              Text('0300-8900392',style: TextStyle(
+                color: const Color.fromARGB(255, 5, 122, 110),
+              ),),
+            ],
           ),
         ),
-      )
+
+        SizedBox(height: 20,),
+         Container(
+          height:35,
+          width: 300,
+          
+          decoration: BoxDecoration(
+          color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [BoxShadow(
+              color: const Color.fromARGB(255, 15, 22, 220),
+                spreadRadius: 3,
+              blurRadius: 3,
+            )]
+          ),
+          
+          child:  Row(
+            children: [
+              SizedBox(width: 10,),
+              Icon(Icons.email,color: const Color.fromARGB(255, 3, 120, 108),),
+              SizedBox(width: 5,),
+              Text('abc@gmail.com',style: TextStyle(
+                color: const Color.fromARGB(255, 5, 122, 110),
+              ),),
+            ],
+          ),
+        ),
+        Divider(),
+         
+
+          
+          
+          
+          ],),
+        ),
+      ),
+        
+      
+
+      
     );
   }
 }
