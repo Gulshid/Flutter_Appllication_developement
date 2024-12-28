8cf138b17cac08161d51ac042c4e5070442e9191
Gulshid Zada <gulshidzada4@gmail.com>
2024-12-28 09:04:02 +0500

    Ui 19

---
+++ UI/UX/flutter_application_1/UI_20.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UI_20 extends StatefulWidget {
+  const UI_20({super.key});
+
+  @override
+  State<UI_20> createState() => _UI_20State();
+}
+
+class _UI_20State extends State<UI_20> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
