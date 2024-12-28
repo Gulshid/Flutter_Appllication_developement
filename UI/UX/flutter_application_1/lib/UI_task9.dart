a504126a7ba8162a39bf974d28e5519016b16760
Gulshid Zada <gulshidzada4@gmail.com>
2024-12-28 15:42:24 +0500

    Task 9

--- UI/UX/flutter_application_1/lib/UI_task9.dart
+++ UI/UX/flutter_application_1/lib/UI_task9.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -10,44 +10,123 @@ class Task9 extends StatefulWidget {
 }
 
 class _Task9State extends State<Task9> {
+  List colors=[Colors.white,
+  Colors.white,
+  Colors.white,
+  Colors.white,
+  Colors.white,
+  Colors.white,
+  ];
+  List img=['assets/img_1.jpg',
+  'assets/img_2.jpg',
+  'assets/img_3.jpg',
+  'assets/img_4.jpg',
+  'assets/img_5.jpg',
+  'assets/img_6.jpg'];
+
+  List name=['MCQs','Quis','papers','PDF','Jobs','About'];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 21, 6, 121),
-        leading: Icon(Icons.menu,color: Colors.black,size: 30,),
-        actions: [Container(
-          height: 50,
-          width: 50,
-          decoration: BoxDecoration(
-            color: Colors.white,
-             borderRadius: BorderRadius.circular(15),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 21, 6, 121),
+          leading: Icon(
+            Icons.menu,
+            color: const Color.fromARGB(255, 247, 244, 244),
+            size: 30,
           ),
-          child: ClipRRect(
-           
-            child: Image.asset('assets/img_1.jpg',height: 50,width: 50,),borderRadius: BorderRadius.circular(15),
-          ),
-        ),SizedBox(width: 40,),],
-      ),
-
-    body:Column(children: [
-        Container(
-          height: 200,
-          width: 500,
-          color: Colors.blue,
-          child: Row(
-            children: [
-              Text('Dashboard',style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+          actions: [
+            Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
                 color: Colors.white,
-              ),)
-            ],
-          ),
-        )
-
-    ],)
-      
-    );
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: ClipRRect(
+                child: Image.asset(
+                  'assets/img_1.jpg',
+                  height: 50,
+                  width: 50,
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            SizedBox(
+              width: 40,
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 80,
+              width: 500,
+              color: const Color.fromARGB(255, 6, 21, 121),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Dashboard',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'last Update:23 Aug 2023',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromARGB(255, 143, 137, 137),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Expanded(
+              child: GridView.builder( itemCount: colors.length,
+          gridDelegate:
+              // ignore: prefer_const_constructors
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                
+               
+                itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: colors[index],
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [BoxShadow(
+                        color: const Color.fromARGB(255, 8, 75, 130),
+                        spreadRadius: 4,
+                        blurRadius: 4,
+                      )]
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(img[index],),
+                        Text(name[index]),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+            )
+          ],
+        ));
   }
 }
