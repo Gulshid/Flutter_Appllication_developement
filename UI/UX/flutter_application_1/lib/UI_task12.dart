2cb52ee1bd32cf95b3c3908abd1227c209036fe1
Gulshid Zada <gulshidzada4@gmail.com>
2025-01-01 12:45:11 -0800

    update

--- UI/UX/flutter_application_1/lib/UI_task12.dart
+++ UI/UX/flutter_application_1/lib/UI_task12.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/UI_task10.dart';
+//import 'package:flutter_application_1/UI_task6.dart';
+import 'package:flutter_application_1/UI_task8.dart';
+//import 'package:flutter_application_1/UI_task9.dart';
 //import 'package:flutter_application_1/UI_task6.dart';
 
 class Task12 extends StatefulWidget {
@@ -14,33 +18,92 @@ class _Task12State extends State<Task12> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Center(
-            child: Text(
-              'Login & Register ',
-              style: TextStyle(
-                fontSize: 25,
-                color: const Color.fromARGB(255, 49, 223, 130),
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-              ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/img_2.jpg',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
-        backgroundColor: Colors.white,
-        body: 
-        
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: 
-              AssetImage('assets/img_11.jpeg'),
-              fit: BoxFit.cover,
-              ),
-            ) ,
+          SizedBox(
+            height: 500,
           ),
-        )
-        );
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Task10()));
+                  },
+                  child: Container(
+                    height: 60,
+                    width: 500,
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 14, 177, 20),
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Instagram_UI()));
+                  },
+                  child: Container(
+                    height: 60,
+                    width: 500,
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
