23c2bb9243904feb63c859d4f01464c128cee175
Gulshid Zada <gulshidzada4@gmail.com>
2025-01-02 10:22:42 -0800

    update

--- UI/UX/flutter_application_1/lib/UI_task12(b).dart
+++ UI/UX/flutter_application_1/lib/UI_task12(b).dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/UI_task13.dart';
 
 class page_2 extends StatefulWidget {
   const page_2({super.key});
@@ -13,177 +14,237 @@ class _page_2State extends State<page_2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: Container(
-            height: 15,
-            width: 15,
-            decoration: BoxDecoration(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Container(
+          height: 15,
+          width: 15,
+          decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadiusDirectional.circular(10),
               // ignore: prefer_const_constructors
-              boxShadow: [BoxShadow(
-                color: const Color.fromARGB(255, 135, 161, 183),
-                blurRadius: 4,
-                spreadRadius: 5,
-              )]
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 135, 161, 183),
+                  blurRadius: 4,
+                  spreadRadius: 5,
+                )
+              ]),
+          child: Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.black,
+            size: 20,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20,
             ),
-            child: Icon(Icons.arrow_back_ios_new,color: Colors.black,size: 20,),
-          ),),
-backgroundColor: Colors.white,
-          body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20,),
-              Text('Hello! Register to get\nstarted',style: TextStyle(
+            Text(
+              'Hello! Register to get\nstarted',
+              style: TextStyle(
                 color: Colors.black,
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
                 fontStyle: FontStyle.normal,
-              
-              ),),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                 prefixIcon: Icon(Icons.text_decrease),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                  hintText: 'Username',
-                ),
-
-                
               ),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.email,color: Colors.black,),
-                  hintText: 'Enter Your Email',
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.text_decrease),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                hintText: 'Username',
               ),
-              SizedBox(height: 10,),
-              
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.password,color:  Colors.black,),
-                  suffixIcon: Icon(Icons.remove_red_eye),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                  hintText: 'Password',
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Colors.black,
                 ),
-
-                
+                hintText: 'Enter Your Email',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
-              SizedBox(height: 10,),
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.password,color: Colors.black,),
-                  suffixIcon: Icon(Icons.remove_red_eye),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                  hintText: 'Confirm Password',
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.password,
+                  color: Colors.black,
                 ),
-
-                
+                suffixIcon: Icon(Icons.remove_red_eye),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                hintText: 'Password',
               ),
-          
-
-              SizedBox(height: 30,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.password,
+                  color: Colors.black,
+                ),
+                suffixIcon: Icon(Icons.remove_red_eye),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                hintText: 'Confirm Password',
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Task13()));
+                },
                 child: Container(
                   height: 60,
                   width: 500,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadiusDirectional.circular(10),
-                    color: Colors.black,
-                    boxShadow: [BoxShadow(
-                      color: const Color.fromARGB(255, 4, 85, 151),
-                      blurRadius: 3,
-                      spreadRadius: 4,
-                    )]
-                  ),
+                      borderRadius: BorderRadiusDirectional.circular(10),
+                      color: Colors.black,
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 4, 85, 151),
+                          blurRadius: 3,
+                          spreadRadius: 4,
+                        )
+                      ]),
                   child: Center(
-                    child: Text('Register',style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),),
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
-                
               ),
-              SizedBox(height: 15,),
-              Center(
-                child: Text('--------------or Register with----------------',style: TextStyle(
-                  color:const Color.fromARGB(255, 134, 123, 123),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: Text(
+                '--------------or Register with----------------',
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 134, 123, 123),
                   fontSize: 20,
-                  
-                ),),
+                ),
               ),
-              SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    height: 50,
-                    width: 60,
-                    decoration: BoxDecoration(
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: 50,
+                  width: 60,
+                  decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(6),
-                      boxShadow: [BoxShadow(
-                        color: Colors.blue,
-                        spreadRadius: 3,
-                        blurRadius: 4,
-                      )]
-                    ),
-                    child: ClipRRect(
-                      child: Image.asset('assets/img.webp',fit:BoxFit.cover,),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blue,
+                          spreadRadius: 3,
+                          blurRadius: 4,
+                        )
+                      ]),
+                  child: ClipRRect(
+                    child: Image.asset(
+                      'assets/img.webp',
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  Container(  height: 50,
-                    width: 60,
-                    decoration: BoxDecoration(
+                ),
+                Container(
+                  height: 50,
+                  width: 60,
+                  decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(6),
-                      boxShadow: [BoxShadow(
-                        color: Colors.blue,
-                        spreadRadius: 3,
-                        blurRadius: 4,
-                      )]
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blue,
+                          spreadRadius: 3,
+                          blurRadius: 4,
+                        )
+                      ]),
+                  child: ClipRRect(
+                    child: Image.asset(
+                      'assets/img_9.webp',
+                      fit: BoxFit.cover,
                     ),
-                    child: ClipRRect(
-                      child: Image.asset('assets/img_9.webp',fit:BoxFit.cover,),
-                    ),
-                    ),
-                  Container(  height: 50,
-                    width: 60,
-                    decoration: BoxDecoration(
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 60,
+                  decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(6),
-                      boxShadow: [BoxShadow(
-                        color: Colors.blue,
-                        spreadRadius: 3,
-                        blurRadius: 4,
-                      )]
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blue,
+                          spreadRadius: 3,
+                          blurRadius: 4,
+                        )
+                      ]),
+                  child: ClipRRect(
+                    child: Image.asset(
+                      'assets/img_8.png',
                     ),
-                    child: ClipRRect(
-                      child: Image.asset('assets/img_8.png',),
-                    ),
-                    ),
-                ],
-              ),
-              SizedBox(height: 120,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-              
-                children: [
-                  Text("Already  have an account?",style: TextStyle(color: Colors.black),),
-              Text('Login Now.',style: TextStyle(color: const Color.fromARGB(255, 40, 123, 192)),)
-                ],
-              )
-            ],
-          ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already  have an account?",
+                  style: TextStyle(color: Colors.black),
+                ),
+                Text(
+                  'Login Now.',
+                  style:
+                      TextStyle(color: const Color.fromARGB(255, 40, 123, 192)),
+                )
+              ],
+            )
+          ],
         ),
+      ),
     );
   }
 }
--- UI/UX/flutter_application_1/lib/UI_task13(b).dart
+++ UI/UX/flutter_application_1/lib/UI_task13(b).dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/UI_task14.dart';
 
 class page2 extends StatefulWidget {
   const page2({super.key});
@@ -13,161 +14,200 @@ class _page2State extends State<page2> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: Icon(Icons.arrow_back_ios,),
+        leading: Icon(
+          Icons.arrow_back_ios,
+        ),
         title: Text('Puma'),
-        actions: [Icon(Icons.menu),
-        SizedBox(width: 20,)],
+        actions: [
+          Icon(Icons.menu),
+          SizedBox(
+            width: 20,
+          )
+        ],
       ),
-
-              backgroundColor: Colors.white,
-
-    body:Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 20),
-      child: Container(
-        height: 500,
-        width: 450,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [BoxShadow(
-            color: Colors.grey,
-            spreadRadius: 3,
-            blurRadius: 3,
-          )]
-        ),
-        
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('assets/img_13.png'),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              height: 30,
-              width: 50,
-              decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(18),
-          boxShadow: [BoxShadow(
-            color: Colors.grey,
-            spreadRadius: 3,
-            blurRadius: 3,
-          )]
-        ),
-        child: Center(child: Text('Size',style: TextStyle(color: const Color.fromARGB(255, 223, 51, 39)),)),
-            ),
-
-
-          SizedBox(height: 10,),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        child: Container(
+          height: 500,
+          width: 450,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  spreadRadius: 3,
+                  blurRadius: 3,
+                )
+              ]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/img_13.png'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 30,
+                width: 50,
+                decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(25),
-                    boxShadow: [BoxShadow(
-                      color: Colors.grey,
-                      spreadRadius: 3,
-                      blurRadius: 4,
-                    )]
-                  ),
-                  child: Center(
-                    child: Text('32',style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),),
+                    borderRadius: BorderRadius.circular(18),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: 3,
+                        blurRadius: 3,
+                      )
+                    ]),
+                child: Center(
+                    child: Text(
+                  'Size',
+                  style:
+                      TextStyle(color: const Color.fromARGB(255, 223, 51, 39)),
+                )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 3,
+                              blurRadius: 4,
+                            )
+                          ]),
+                      child: Center(
+                        child: Text(
+                          '32',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 3,
+                              blurRadius: 4,
+                            )
+                          ]),
+                      child: Center(
+                        child: Text(
+                          '33',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 3,
+                              blurRadius: 4,
+                            )
+                          ]),
+                      child: Center(
+                        child: Text(
+                          '34',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromARGB(255, 230, 55, 43),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 3,
+                              blurRadius: 4,
+                            )
+                          ]),
+                      child: Center(
+                        child: Text(
+                          '35',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 400,
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 212, 28, 15),
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.red,
+                        spreadRadius: 3,
+                        blurRadius: 3,
+                      )
+                    ]),
+                child: Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Task14()));
+                    },
+                    child: Text(
+                      'Buy Now',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
-                Container( height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(25),
-                    boxShadow: [BoxShadow(
-                      color: Colors.grey,
-                      spreadRadius: 3,
-                      blurRadius: 4,
-                    )]
-                  ),
-                  child: Center(
-                    child: Text('33',style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),),
-                  ),),
-                Container( height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(25),
-                    boxShadow: [BoxShadow(
-                      color: Colors.grey,
-                      spreadRadius: 3,
-                      blurRadius: 4,
-                    )]
-                  ),
-                  child: Center(
-                    child: Text('34',style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: const Color.fromARGB(255, 230, 55, 43),
-                    ),),
-                  ),),
-                Container( height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(25),
-                    boxShadow: [BoxShadow(
-                      color: Colors.grey,
-                      spreadRadius: 3,
-                      blurRadius: 4,
-                    )]
-                  ),
-                  child: Center(
-                    child: Text('35',style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),),
-                  ),),
-              ],
-            ),
+              )
+            ],
           ),
-
-          Container(
-            height: 50,
-            width: 400,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 212, 28, 15),
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [BoxShadow(
-                color: Colors.red,
-                spreadRadius: 3,
-                blurRadius: 3,
-              )]
-            ),
-            child: Center(
-              child: Text('Buy Now',style: TextStyle(
-                color: Colors.white
-              ),),
-            ),
-          )
-          ],
         ),
-
-        
       ),
-    ) ,
     );
   }
 }
