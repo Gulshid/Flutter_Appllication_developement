014c8d1b2b6a0d24755ea36549acf59b7a35d121
Gulshid Zada <gulshidzada4@gmail.com>
2025-01-09 08:37:07 +0500

    update

---
+++ UI/UX/flutter_application_1/lib/UI_23.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Spalash_screen extends StatefulWidget {
+  const Spalash_screen({super.key});
+
+  @override
+  State<Spalash_screen> createState() => _Spalash_screenState();
+}
+
+class _Spalash_screenState extends State<Spalash_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
