c62302c6c797b246f71bdc5a117175cae19c3381
Gulshid Zada <gulshidzada4@gmail.com>
2025-01-10 18:39:43 +0500

    update

--- UI/UX/flutter_application_1/lib/main.dart
+++ UI/UX/flutter_application_1/lib/main.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/UI_task15.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_application_1/UI_26.dart';
+//import 'package:flutter_application_1/main.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:Task15(),
-      // 
+    
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+   
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Dev',
+        
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: UI_26(),
+        );
+      },
+    
     );
-
   }
 }
