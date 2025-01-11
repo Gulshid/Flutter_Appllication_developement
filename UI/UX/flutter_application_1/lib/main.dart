b8cc164c7380898f2a12179bc7f58fca4cd4b61f
Gulshid Zada <gulshidzada4@gmail.com>
2025-01-11 13:01:12 +0500

    update

--- UI/UX/flutter_application_1/lib/main.dart
+++ UI/UX/flutter_application_1/lib/main.dart
@@ -1,37 +1,37 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_application_1/UI_26.dart';
-//import 'package:flutter_application_1/main.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutter_application_1/UI_26.dart';
+// //import 'package:flutter_application_1/main.dart';
 
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
+//   @override
+//   Widget build(BuildContext context) {
     
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
+//     return ScreenUtilInit(
+//       designSize: const Size(360, 690),
+//       minTextAdapt: true,
+//       splitScreenMode: true,
    
-      builder: (_, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter Dev',
+//       builder: (_, child) {
+//         return MaterialApp(
+//           debugShowCheckedModeBanner: false,
+//           title: 'Flutter Dev',
         
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
-          ),
-          home: UI_26(),
-        );
-      },
+//           theme: ThemeData(
+//             primarySwatch: Colors.blue,
+//             textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+//           ),
+//           home: UI_26(),
+//         );
+//       },
     
-    );
-  }
-}
+//     );
+//   }
+// }
