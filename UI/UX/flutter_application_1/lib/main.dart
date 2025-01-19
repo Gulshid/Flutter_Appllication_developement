4a8960010c0bc4b48d213d5af00b0c85f479ec70
Gulshid Zada <gulshidzada4@gmail.com>
2025-01-19 18:57:28 +0500

    update

--- UI/UX/flutter_application_1/lib/main.dart
+++ UI/UX/flutter_application_1/lib/main.dart
@@ -1,37 +1,33 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:flutter_application_1/UI_26.dart';
-// //import 'package:flutter_application_1/main.dart';
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:http/http.dart' as http;
+import 'package:flutter_application_1/project/page1.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+void main() {
+  runApp(const MyApp());
+}
 
-// void main() {
-//   runApp(const MyApp());
-// }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-    
-//     return ScreenUtilInit(
-//       designSize: const Size(360, 690),
-//       minTextAdapt: true,
-//       splitScreenMode: true,
-   
-//       builder: (_, child) {
-//         return MaterialApp(
-//           debugShowCheckedModeBanner: false,
-//           title: 'Flutter Dev',
-        
-//           theme: ThemeData(
-//             primarySwatch: Colors.blue,
-//             textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
-//           ),
-//           home: UI_26(),
-//         );
-//       },
-    
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Dev',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: pa_1(),
+        );
+      },
+    );
+  }
+}
---
+++ UI/UX/flutter_application_1/lib/project/sc4.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class sc4 extends StatelessWidget {
+  const sc4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.h,
+      width: 100.h,
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 243, 33, 68),
+        borderRadius: BorderRadius.circular(12.r),
+      ),
+    );
+  }
+}
