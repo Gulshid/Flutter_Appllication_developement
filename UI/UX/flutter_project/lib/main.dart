e4305f920eb829d1117b77894ac940c6d1a4373c
Gulshid Zada <gulshidzada4@gmail.com>
2025-01-28 17:08:35 +0500

    update pakages

--- UI/UX/flutter_project/lib/main.dart
+++ UI/UX/flutter_project/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/Flutter%20APi/Api_Models/practise_api.dart';
+import 'package:flutter_application_1/Flutter%20APi/get_100_post_api.dart';
 // import 'package:flutter_application_1/Flutter%20APi/get_100_post_api.dart';
-import 'package:flutter_application_1/Flutter%20APi/get_practise.dart';
+
 
 // ignore: unused_import
 import 'package:http/http.dart' as http;
@@ -28,7 +30,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.blue,
             textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
           ),
-          home:practise_get(),
+          home:post_api(),
         );
       },
     );
