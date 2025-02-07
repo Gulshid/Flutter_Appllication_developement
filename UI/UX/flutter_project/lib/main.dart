3ff7c03e354cfe8d5363163c1b943f77cd26978e
Gulshid Zada <gulshidzada4@gmail.com>
2025-02-07 11:52:52 +0500

    update

--- UI/UX/flutter_project/lib/main.dart
+++ UI/UX/flutter_project/lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-// ignore: unnecessary_import
 import 'package:flutter/cupertino.dart';
-// import 'package:flutter_application_1/Flutter%20APi/Api_Tasks/Edit_profile.dart';
 import 'package:flutter_application_1/Flutter%20APi/Api_Tasks/Splash_Screen.dart';
-// import 'package:flutter_application_1/Flutter%20APi/Api_Tasks/Splash_Screen.dart';
-
-
-// import 'package:http/http.dart' as http;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-// ignore: unused_import
 
 void main() {
   runApp(const MyApp());
