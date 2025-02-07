740a034505aac4c7a14b1e191bd7344e59a427fe
Gulshid Zada <gulshidzada4@gmail.com>
2025-02-07 21:48:45 +0500

    done of signed and login screen

--- UI/UX/flutter_project/lib/Flutter APi/Api_Tasks/Task_login.dart
+++ UI/UX/flutter_project/lib/Flutter APi/Api_Tasks/Task_login.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class task_login extends StatefulWidget {
   const task_login({super.key});
@@ -10,21 +14,56 @@ class task_login extends StatefulWidget {
 }
 
 class _task_loginState extends State<task_login> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
   @override
+  void initState() {
+    super.initState();
+    Local_Database();
+  }
+
+//local Databae(Shared Preference)...
+  Future<void> Local_Database() async {
+    SharedPreferences value = await SharedPreferences.getInstance();
+    email.text = value.getString('email') ?? '';
+    password.text = value.getString('password') ?? '';
+  }
+
+//for post the message
+  Future<void> login_user() async {
+    final response =
+        await http.post(Uri.parse('https://reqres.in/api/users'), body: {
+      'email': email.text,
+      'password': password.text,
+    });
+    if (response.statusCode == 201) {
+      var data = jsonDecode(response.body);
+      print('Email-------->${data['email']}');
+      print('Password-------->${data['password']}');
+      print('Account is Sucessfully loged ---------->');
+
+      SharedPreferences value = await SharedPreferences.getInstance();
+      value.setString('email', email.text);
+      value.setString('password', email.text);
+    } else {
+      print('login is failed due to some issue ');
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Stack(
-        clipBehavior: Clip.none,
-        children:[
-          
-          Positioned.fill(child: Image.asset('assets/img_5.jpg',fit: BoxFit.cover,)),
-         Column(
+      body: Stack(clipBehavior: Clip.none, children: [
+        Positioned.fill(
+            child: Image.asset(
+          'assets/img_5.jpg',
+          fit: BoxFit.cover,
+        )),
+        Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-           
-         
             SizedBox(
               height: 120.h,
             ),
@@ -40,76 +79,114 @@ class _task_loginState extends State<task_login> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
               child: TextField(
+                controller: email,
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15.r),
-                    borderSide: BorderSide(color: const Color.fromARGB(255, 39, 26, 154),width: 2.w,),
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 39, 26, 154),
+                      width: 2.w,
+                    ),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12.r),
-                    borderSide: BorderSide(color: const Color.fromARGB(255, 130, 19, 164),width: 2.w),
+                    borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 130, 19, 164),
+                        width: 2.w),
                   ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.r),
-                      borderSide: BorderSide(color: const Color.fromARGB(255, 10, 142, 160),
-                      width: 200.w,
-                      strokeAlign: BorderSide.strokeAlignCenter,
-                      style: BorderStyle.solid
-                      ),
-                    ), 
-                    labelText: 'Enter Email',contentPadding: EdgeInsets.symmetric(horizontal: 80.0,vertical: 30.0),floatingLabelAlignment: FloatingLabelAlignment.start,floatingLabelBehavior: FloatingLabelBehavior.auto,labelStyle: GoogleFonts.agbalumo(color: Colors.white,fontSize: 15.sp,),),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.r),
+                    borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 10, 142, 160),
+                        width: 200.w,
+                        strokeAlign: BorderSide.strokeAlignCenter,
+                        style: BorderStyle.solid),
+                  ),
+                  labelText: 'Enter Email',
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 80.0, vertical: 30.0),
+                  floatingLabelAlignment: FloatingLabelAlignment.start,
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  labelStyle: GoogleFonts.agbalumo(
+                    color: Colors.white,
+                    fontSize: 15.sp,
+                  ),
+                ),
               ),
             ),
-        
             SizedBox(height: 40.h),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
               child: TextField(
+                controller: password,
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15.r),
-                    borderSide: BorderSide(color: const Color.fromARGB(255, 67, 25, 172),width: 2.w,),
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 67, 25, 172),
+                      width: 2.w,
+                    ),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12.r),
-                    borderSide: BorderSide(color: const Color.fromARGB(255, 19, 106, 164),width: 2.w),
+                    borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 19, 106, 164),
+                        width: 2.w),
                   ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.r),
-                      borderSide: BorderSide(color: const Color.fromARGB(255, 10, 142, 160),
-                      width: 200.w,
-                      strokeAlign: BorderSide.strokeAlignCenter,
-                      style: BorderStyle.solid
-                      ),
-                    ), 
-                    labelText: 'Enter Password',contentPadding: EdgeInsets.symmetric(horizontal: 80.0,vertical: 30.0),floatingLabelAlignment: FloatingLabelAlignment.start,floatingLabelBehavior: FloatingLabelBehavior.auto,labelStyle: GoogleFonts.agbalumo(color: Colors.white,fontSize: 15.sp,),),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.r),
+                    borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 10, 142, 160),
+                        width: 200.w,
+                        strokeAlign: BorderSide.strokeAlignCenter,
+                        style: BorderStyle.solid),
+                  ),
+                  labelText: 'Enter Password',
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 80.0, vertical: 30.0),
+                  floatingLabelAlignment: FloatingLabelAlignment.start,
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  labelStyle: GoogleFonts.agbalumo(
+                    color: Colors.white,
+                    fontSize: 15.sp,
+                  ),
+                ),
               ),
             ),
-        
-            SizedBox(height: 40.h,),
+            SizedBox(
+              height: 40.h,
+            ),
             Center(
-              child: Container(
-                height: 50.h,
-                width: 300.w,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(25.r),
-                  boxShadow: [BoxShadow(
-                    color: Colors.black,
-                    blurRadius: 4,
-                    spreadRadius: 4,
-                    offset: Offset(1,2),
-                    blurStyle: BlurStyle.normal
-                  )]
+              child: GestureDetector(
+                onTap: () {
+                  login_user();
+                },
+                child: Container(
+                  height: 50.h,
+                  width: 300.w,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25.r),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 4,
+                            spreadRadius: 4,
+                            offset: Offset(1, 2),
+                            blurStyle: BlurStyle.normal)
+                      ]),
+                  child: Center(
+                      child: Text(
+                    'login Now',
+                    style: GoogleFonts.agbalumo(
+                        fontSize: 20.sp, color: Colors.white),
+                  )),
                 ),
-              
-                child: Center(child: Text('login Now',style: GoogleFonts.agbalumo(fontSize: 20.sp,color: Colors.white),)),
               ),
             )
           ],
         ),
-        ]
-      ),
+      ]),
     );
   }
 }
