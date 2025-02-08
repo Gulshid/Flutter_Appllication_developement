4ea97628c711e799287d8bf4c58ebb60eb3d4709
Gulshid Zada <gulshidzada4@gmail.com>
2025-02-08 17:47:32 +0500

    update

---
+++ UI/UX/flutter_project/lib/Flutter UI/UI_Animation_2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Animation_2 extends StatefulWidget {
+  const Animation_2({super.key});
+
+  @override
+  State<Animation_2> createState() => _Animation_2State();
+}
+
+class _Animation_2State extends State<Animation_2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
