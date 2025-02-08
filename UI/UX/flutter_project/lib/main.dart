ef5786f62ee759d3dbe795982668fa0644c839c2
Gulshid Zada <gulshidzada4@gmail.com>
2025-02-08 22:56:19 +0500

    Animation 3 Done

--- UI/UX/flutter_project/lib/main.dart
+++ UI/UX/flutter_project/lib/main.dart
@@ -1,7 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_application_1/Flutter%20UI/UI_Animation_2.dart';
+// import 'package:flutter_application_1/Flutter%20UI/UI_Animation_2.dart';
+import 'package:flutter_application_1/Flutter%20UI/UI_Animation_3.dart';
 // import 'package:flutter_application_1/Flutter%20APi/Api_Tasks/Task_login.dart';
 // import 'package:flutter_application_1/Flutter%20UI/UI_Amimation.dart';
 // import 'package:flutter_application_1/Flutter%20UI/practise.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.blue,
             textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
           ),
-          home:Animation_2(),
+          home:Animation_3(),
         );
       },
     );
